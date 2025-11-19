325c148755414334436937a85fafcd70568fe857
HajarFazillah <hajar.fazillah@gmail.com>
2025-11-19 23:51:15 +0900

    update HeartButton.js

--- ui/HeartButton.js
+++ ui/HeartButton.js
@@ -1,8 +1,25 @@
 export default class HeartButton {
   constructor(scene, x, y) {
+    this.scene = scene;
+    this.isLiked = false; // track state
+
     this.heart = scene.add.text(x, y, '❤', {
       fontSize: '64px',
       color: '#000000'
-    }).setOrigin(1, 0); // top-right anchor
+    })
+      .setOrigin(1, 0)
+      .setInteractive({ useHandCursor: true });
+
+    // === Click Event ===
+    this.heart.on('pointerdown', () => {
+      this.toggleHeart();
+    });
+  }
+
+  toggleHeart() {
+    this.isLiked = !this.isLiked;
+
+    // Change color on toggle
+    this.heart.setColor(this.isLiked ? '#ff0000' : '#000000');
   }
 }

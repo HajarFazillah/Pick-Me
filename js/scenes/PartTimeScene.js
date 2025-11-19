d7ccbdcf8b58e1da7169e3548714259708ab8c57
HajarFazillah <hajar.fazillah@gmail.com>
2025-11-19 23:29:14 +0900

    update PartTimeScene.js

--- js/scenes/PartTimeScene.js
+++ js/scenes/PartTimeScene.js
@@ -1,8 +1,13 @@
 import * as Phaser from 'https://cdn.jsdelivr.net/npm/phaser@3/dist/phaser.esm.js';
+
 import TopButtonBar from '/ui/TopButtonBar.js';
 import BottomNavBar from '/ui/BottomNavBar.js';
+import PartTimeFrame from '/ui/PartTimeFrame.js';
+import HeartButton from '/ui/HeartButton.js';
 
-export default class GameScene extends Phaser.Scene {
+import LargeClickButton from '/ui/LargeClickButton.js';
+
+export default class PartTimeScene extends Phaser.Scene {
   constructor() {
     super('PartTimeScene');
   }
@@ -10,26 +15,38 @@ export default class GameScene extends Phaser.Scene {
   preload() {}
 
   create() {
-    this.cameras.main.setBackgroundColor('#3d3d3d');
+    console.log("Loaded:", this.scene.key);
 
-    // Store game objects so we can reposition them
-    this.title = this.add.text(this.cameras.main.centerX, 100, 'Part-Time Scene', { 
-        fontSize: '48px', 
-        color: '#ffffff' 
-    }).setOrigin(0.5);
+    const cx = this.cameras.main.centerX;
+    const cy = this.cameras.main.centerY;
 
-    // This scene needs its own BottomNavBar to allow navigation
-    this.bottomNavBar = new BottomNavBar(this, 1100);
+    this.cameras.main.setBackgroundColor('#ffffff');
 
-    this.scale.on('resize', this.resize, this);
-  }
+    // === Top UI Bar ===
+    new TopButtonBar(this, cx - 240, 40);
 
-    resize(gameSize, baseSize, displaySize, resolution) {
-        this.title.setPosition(this.cameras.main.centerX, 100);
+    // === Count Coin ===
+    //this.countCoin = new CountCoin(this, cx, 120);
+    //this.countCoin.setProgress(0.4);
 
-        if(this.bottomNavBar) {
-            this.bottomNavBar.destroy();
-        }
-        this.bottomNavBar = new BottomNavBar(this, 1100);
+    // === Three small boxes ===
+    let boxStartX = cx - 150;
+    for (let i = 0; i < 3; i++) {
+      this.add.rectangle(boxStartX + i * 60, 280, 40, 40, 0xcccccc)
+        .setStrokeStyle(2, 0x000000)
+        .setOrigin(0.5);
     }
+
+    // === Large Image Frame ===
+    this.frame = new PartTimeFrame(this, cx, cy - 100, 350, 500);
+
+    // === Heart in top-right of frame ===
+    this.heart = new HeartButton(this, cx + 160, cy - 330);
+
+    // === CLICK Button ===
+    this.clickButton = new LargeClickButton(this, cx, cy + 300);
+
+    // === Bottom Navigation ===
+    this.bottomNav = new BottomNavBar(this);
+  }
 }
